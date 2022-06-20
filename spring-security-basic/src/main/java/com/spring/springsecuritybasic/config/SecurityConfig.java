7793b8d10ebea2a7b2d64fad86277befb9684479
hyun0404woo <hyun0404woo@gmail.com>
2022-06-20 23:31:24 +0900

    Feat: Security Path Config
    - login : permitAll
    - signin : permitAll
    - /auth/** : only user, admin
    - /test/** : only manager, admin
    - add jwtFilter

--- spring-security-basic/src/main/java/com/spring/springsecuritybasic/config/SecurityConfig.java
+++ spring-security-basic/src/main/java/com/spring/springsecuritybasic/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.spring.springsecuritybasic.config;
 
+import com.spring.springsecuritybasic.config.jwt.JwtAuthorizationFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -20,10 +22,15 @@ public class SecurityConfig {
 
     @Autowired
     private CorsConfig corsConfig;
+
+    @Autowired
+    private JwtAuthorizationFilter jwtFilter;
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         LOG.info("Custom Filter Chain Bean 생성");
+//        JwtAuthorizationFilter jwtFilter = new JwtAuthorizationFilter();
+        
         http
                 .addFilter(corsConfig.corsFilter())
                 .csrf().disable()
@@ -31,9 +38,13 @@ public class SecurityConfig {
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/api/auth/login").permitAll()
+                .antMatchers("/api/auth/signin").permitAll()
                 .antMatchers("/api/auth/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/test/**")
                 .access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
                 .anyRequest().permitAll();
         
