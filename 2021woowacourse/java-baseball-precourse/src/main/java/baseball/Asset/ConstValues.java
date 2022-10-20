7e5ac7e4e5332fb831b26f49e9b4bd96a93b3cc1
hyun0404woo <hyun0404woo@gmail.com>
2022-10-20 23:04:06 +0900

    feat: 상수 값 저장 클래스 생성

---
+++ 2021woowacourse/java-baseball-precourse/src/main/java/baseball/Asset/ConstValues.java
@@ -0,0 +1,11 @@
+package baseball.Asset;
+
+public class ConstValues {
+    public final static int START_NUM = 1;
+    public final static int END_NUM = 9;
+    public final static int EXCLUSIVE_END_NUM = 10;
+    public final static int BALL_LENGTH = 3;
+
+    public final static String EMPTY_STR = "";
+    
+}
